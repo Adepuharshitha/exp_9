096ebdd48cb4660fe707293f391676162f7b0ccc
harshitha <adepuharshitha56@gmail.com>
2025-10-21 18:05:56 +0530

    Initial commit for exp_9

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM openjdk:17 
+WORKDIR /app
+COPY helloworld.java/helloworld.java
+RUN javac helloworld.java 
+CMD ["java", "helloworld"] 
---
+++ helloworld.java
@@ -0,0 +1,6 @@
+public class helloworld{
+    public static void main(String[] args){
+        String name="adepuharshitha";
+        System.out.println("my name is" +name);
+    }
+}
